8f5ae175079ecdc7a47e242e0baf17499bd14f7d
CopmuterManDanEx <di236'exeter.ac.uk>
2023-11-13 14:29:46 +0000

    Flake 8 fixes

--- source/models.py
+++ source/models.py
@@ -1,12 +1,17 @@
 import numpy as np
 
-#This is a function that computes the SIR model right hand side
+# This is a function that computes the SIR model right hand side
+
+
 def SIR(Y, r, a):
     S, I, _ = Y
     return [-r * I * S, r * I * S - a * I, a * I]
 
 
-#The Brusselator function takes a 2D array Y and applies the brusselator function
+# The Brusselator function takes a 2D array Y and applies the brusselator
+# function
+
+
 def Brusselator(Y, a, b):
     x, y = Y
     return np.array([a + x**2 * y - b * x + x, b * x - x**2 * y])
